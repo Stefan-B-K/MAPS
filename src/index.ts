0e4d65a09c8dcf51c5748de48a22b59b05ce2ad2
iStef <stefan_b_k@icloud.com>
2022-07-02 21:10:52 +0300

    finish

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,17 @@ const company = new Company()
 
 const userButton = document.querySelector('#user') as HTMLButtonElement
 const userInfo  = userButton.querySelector('.card-content ') as HTMLLabelElement
-userInfo.innerHTML += 'User: <br>' + user.name
+userInfo.innerHTML +=`<u>User:</u> <h4>${user.name}</h4>`
 const companyButton = document.querySelector('#company') as HTMLButtonElement
 const companyInfo  = companyButton.querySelector('.card-content ') as HTMLLabelElement
-companyInfo.innerHTML += 'Company: <br>' + company.name
+companyInfo.innerHTML += `<u>Company:</u> <h4>${company.name}</h4>`
 userButton.onclick = () => map.centerView(user)
 companyButton.onclick = () => map.centerView(company)
 
 const reloadButton = document.querySelector('#reload ') as HTMLButtonElement
 reloadButton.onclick = () => location.reload()
-
+const reloadPopup = document.querySelector('.reload-popup')
+reloadButton.onmouseover = () => reloadPopup.classList.remove('inactive')
+reloadButton.onmouseleave = () => reloadPopup.classList.add('inactive')
 const map = new Map('map', user.location)
 
